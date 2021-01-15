d9311913f6f0a7def68c052eb75e0b74553d559c
manisha-khandare <manisha.khandare@gmail.com>
2021-01-15 20:13:44 +0530

    commit

--- app/src/main/java/com/example/mvvmretrofitapplication/data/api/RetrofitBuilder.kt
+++ app/src/main/java/com/example/mvvmretrofitapplication/data/api/RetrofitBuilder.kt
@@ -5,8 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitBuilder {
 
-    private const val BASE_URL = "https://5e510330f2c0d300147c034c.mockapi.io/"
-  //  private const val BASE_URL = "https://5e99a9b1bc561b0016af3540.mockapi.io/jet2/api/v1/blogs?page=1&limit=10"
+    private const val BASE_URL = "https://5e99a9b1bc561b0016af3540.mockapi.io/jet2/api/v1/blogs?page=1&limit=10"
 
     private fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
